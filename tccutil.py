bd35e487387983777cad3fd2d7a54547a967aa87
Jacob Salmela <me@jacobsalmela.com>
2020-05-13 19:38:32 -0500

    version bump 1.2.7
    Signed-off-by: Jacob Salmela <me@jacobsalmela.com>

--- tccutil.py
+++ tccutil.py
@@ -11,7 +11,7 @@ from distutils.version import StrictVersion as version
 util_name = os.path.basename(sys.argv[0])
 
 # Utility Version
-util_version = '1.2.6'
+util_version = '1.2.7'
 
 # Current OS X version
 osx_version = version(mac_ver()[0])
